a444ed05ea74208ca0c1d897d3d38d8e1887acfa
chiholinh2001 <chiholinh2001@gmail.com>
2023-02-12 08:58:22 +0700

    model_path

--- run.sh
+++ run.sh
@@ -18,5 +18,4 @@ python3 train.py \
 	--eval_print_freq 100 \
 	--save_freq 10000 \
 	--lr 2e-4 \
-	--model_path /kaggle/working/answer-generation-with-T5/checkpoint/experiment-1/t5-small_0.pth \
 	--backward_freq 16 
